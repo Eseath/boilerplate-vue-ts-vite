5fed93cc57dbbbf7f5b17f95b762da1995080fc4
Ruslan /A <ruslan.a94@ya.ru>
2021-05-27 00:52:54 +0300

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    darkMode: false, // or 'media' or 'class'
+    purge: [
+        './index.html',
+        './src/**/*.{vue,js,ts}',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
